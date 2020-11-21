ea013f762332e4aeb1ab4be4691da4676f5fff0a
sgSillage <51256576+sgSillage@users.noreply.github.com>
2020-11-21 15:14:42 +0800

    Login set

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,75 +1,133 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      autocomplete="on"
+      label-position="left"
+    >
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">欢迎使用 codespace</h3>
       </div>
+      <div id="loginForm">
+        <el-form-item v-if="loginType==='username'||loginType==='forget'||loginType==='register'" prop="username">
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>
+          <el-input
+            ref="username"
+            v-model="loginForm.username"
+            placeholder="Username"
+            name="username"
+            type="text"
+            tabindex="1"
+            autocomplete="on"
+          />
+        </el-form-item>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          autocomplete="on"
-        />
-      </el-form-item>
+        <el-form-item v-if="loginType==='phone'||loginType==='forget'||loginType==='register'" prop="phone">
+          <span class="svg-container">
+            <svg-icon icon-class="mobile" />
+          </span>
+          <el-input
+            ref="phone"
+            v-model="loginForm.phone"
+            placeholder="手机号"
+            name="phone"
+            type="text"
+            tabindex="1"
+            auto-complete="on"
+          />
+        </el-form-item>
 
-      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
-        <el-form-item prop="password">
+        <el-tooltip v-if="loginType==='username'||loginType==='phone'" v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input
+              :key="passwordType"
+              ref="password"
+              v-model="loginForm.password"
+              :type="passwordType"
+              placeholder="Password"
+              name="password"
+              tabindex="2"
+              autocomplete="on"
+              @keyup.native="checkCapslock"
+              @blur="capsTooltip = false"
+              @keyup.enter.native="handleLogin"
+            />
+            <span class="show-pwd" @click="showPwd">
+              <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            </span>
+          </el-form-item>
+        </el-tooltip>
+
+        <el-form-item v-if="loginType==='register'" prop="nickname">
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>
+          <el-input
+            ref="nickname"
+            v-model="loginForm.nickname"
+            placeholder="昵称"
+            name="nickname"
+            type="text"
+            tabindex="1"
+            autocomplete="off"
+          />
+        </el-form-item>
+
+        <el-form-item v-if="loginType==='forget'||loginType==='register'" prop="newPassword">
           <span class="svg-container">
             <svg-icon icon-class="password" />
           </span>
           <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.password"
-            :type="passwordType"
-            placeholder="Password"
-            name="password"
+            ref="newPassword"
+            v-model="loginForm.newPassword"
+            type="text"
+            placeholder="设置密码"
+            name="newPassword"
             tabindex="2"
-            autocomplete="on"
-            @keyup.native="checkCapslock"
-            @blur="capsTooltip = false"
             @keyup.enter.native="handleLogin"
           />
-          <span class="show-pwd" @click="showPwd">
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-          </span>
         </el-form-item>
-      </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div style="position:relative">
-        <div class="tips">
-          <span>Username : admin</span>
-          <span>Password : any</span>
-        </div>
-        <div class="tips">
-          <span style="margin-right:18px;">Username : editor</span>
-          <span>Password : any</span>
-        </div>
-
-        <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-          Or connect with
+        <el-button
+          :loading="loading"
+          type="primary"
+          style="width:100%;margin-bottom:30px;"
+          @click.native.prevent="handleLogin"
+        >
+          <span v-if="loginType==='forget'">重置密码</span>
+          <span v-else-if="loginType==='register'">注册</span>
+          <span v-else>登录</span>
         </el-button>
       </div>
-    </el-form>
 
-    <el-dialog title="Or connect with" :visible.sync="showDialog">
-      Can not be simulated on local, so please combine you own business simulation! ! !
-      <br>
-      <br>
-      <br>
-      <social-sign />
-    </el-dialog>
+      <div style="color: gray">
+        <span v-if="loginType!=='forget'">
+          <el-link @click="loginType='forget'">忘记密码</el-link> |
+        </span>
+        <span v-if="loginType!=='username'">
+          <el-link @click="loginType='username'">用户名登录</el-link> |
+        </span>
+        <span v-if="loginType!=='phone'">
+          <el-link @click="loginType='phone'">手机号登录</el-link> |
+        </span>
+        <span v-if="loginType!=='register'">
+          <el-link @click="loginType='register'">注册</el-link> |
+        </span>
+        <span id="public-about">
+          <el-link href="#/public-about">关于</el-link>
+        </span>
+      </div>
+
+    </el-form>
   </div>
 </template>
 
@@ -98,7 +156,11 @@ export default {
     return {
       loginForm: {
         username: 'admin',
-        password: '111111'
+        password: '111111',
+        phone: '',
+        vcode: '',
+        newPassword: '',
+        nickname: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
@@ -107,8 +169,8 @@ export default {
       passwordType: 'password',
       capsTooltip: false,
       loading: false,
-      showDialog: false,
       redirect: undefined,
+      loginType: 'username',
       otherQuery: {}
     }
   },
@@ -156,7 +218,7 @@ export default {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
           this.loading = true
-          this.$store.dispatch('user/login', this.loginForm)
+          this.$store.dispatch('user/login', { username: this.loginForm.username, password: this.loginForm.password })
             .then(() => {
               this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
               this.loading = false
@@ -248,7 +310,7 @@ $cursor: #fff;
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
+$bg:#6ed4ee;
 $dark_gray:#889aa4;
 $light_gray:#eee;
 
